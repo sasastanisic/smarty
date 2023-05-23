f1c319b036f144a450232b56fa8b1a6711b34549
Sasa <sasastanisic34@gmail.com>
2023-05-23 22:31:20 +0200

    Initialized backend application

---
+++ src/main/java/com/smarty/SmartyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smarty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartyBackendApplication.class, args);
+    }
+
+}
