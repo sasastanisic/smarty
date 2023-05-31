449489502c0a37347a87d85fe22a69ce8ed45b2e
Sasa <sasastanisic4@gmail.com>
2023-05-31 11:06:07 +0200

    Implemented CRUD operations for professor domain

--- src/main/java/com/smarty/domain/professor/service/ProfessorService.java
+++ src/main/java/com/smarty/domain/professor/service/ProfessorService.java
@@ -1,5 +1,21 @@
 package com.smarty.domain.professor.service;
 
+import com.smarty.domain.professor.model.ProfessorRequestDTO;
+import com.smarty.domain.professor.model.ProfessorResponseDTO;
+import com.smarty.domain.professor.model.ProfessorUpdateDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ProfessorService {
+
+    ProfessorResponseDTO createProfessor(ProfessorRequestDTO professorDTO);
+
+    Page<ProfessorResponseDTO> getAllProfessors(Pageable pageable);
+
+    ProfessorResponseDTO getProfessorById(Long id);
+
+    ProfessorResponseDTO updateProfessor(Long id, ProfessorUpdateDTO professorDTO);
+
+    void deleteProfessor(Long id);
 
 }
---
+++ src/main/java/com/smarty/web/ProfessorController.java
@@ -0,0 +1,54 @@
+package com.smarty.web;
+
+import com.smarty.domain.professor.model.ProfessorRequestDTO;
+import com.smarty.domain.professor.model.ProfessorResponseDTO;
+import com.smarty.domain.professor.model.ProfessorUpdateDTO;
+import com.smarty.domain.professor.service.ProfessorService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/professors")
+@PreAuthorize("hasRole('PROFESSOR')")
+public class ProfessorController {
+
+    private final ProfessorService professorService;
+
+    @Autowired
+    public ProfessorController(ProfessorService professorService) {
+        this.professorService = professorService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProfessorResponseDTO> createProfessor(@Valid @RequestBody ProfessorRequestDTO professorDTO) {
+        return ResponseEntity.ok(professorService.createProfessor(professorDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProfessorResponseDTO>> getAllProfessors(Pageable pageable) {
+        return ResponseEntity.ok(professorService.getAllProfessors(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProfessorResponseDTO> getProfessorById(@PathVariable Long id) {
+        return ResponseEntity.ok(professorService.getProfessorById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProfessorResponseDTO> updateProfessor(@PathVariable Long id, @Valid @RequestBody ProfessorUpdateDTO professorDTO) {
+        return ResponseEntity.ok(professorService.updateProfessor(id, professorDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProfessor(@PathVariable Long id) {
+        professorService.deleteProfessor(id);
+    }
+
+}
